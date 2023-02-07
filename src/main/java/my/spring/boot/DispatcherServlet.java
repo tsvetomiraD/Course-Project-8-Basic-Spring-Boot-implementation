d73c8d727571cdfd003e68a111db6ac1113b9d4f
tsvetomiraD <95402102+tsvetomiraD@users.noreply.github.com>
2023-02-07 10:06:20 +0200

    Update DispatcherServlet.java

--- src/main/java/my/spring/boot/DispatcherServlet.java
+++ src/main/java/my/spring/boot/DispatcherServlet.java
@@ -1,5 +1,6 @@
 package my.spring.boot;
 
+import com.google.gson.Gson;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -7,7 +8,11 @@ import my.spring.boot.annotations.PathVariable;
 import my.spring.boot.annotations.RequestBody;
 
 import javax.crypto.spec.OAEPParameterSpec;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
 import java.util.ArrayList;
@@ -16,8 +21,8 @@ import java.util.List;
 import java.util.Map;
 
 public class DispatcherServlet implements Servlet {
-    Map<String, ControllerInfo> controllers = new HashMap<>();
-    Map<String, ControllerInfo> specialControllers = new HashMap<>();
+    Map<String, ControllerInfo> controllers;
+    Map<String, ControllerInfo> specialControllers;
 
     public DispatcherServlet(Map<String, ControllerInfo> controllers, Map<String, ControllerInfo> specialControllers) {
         this.controllers = controllers;
@@ -38,26 +43,57 @@ public class DispatcherServlet implements Servlet {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
         HttpServletRequest request = (HttpServletRequest) servletRequest;
 
-
         String path = request.getPathInfo();
         String requestMethod = request.getMethod().toLowerCase();
         String key = requestMethod + path;
 
         ControllerInfo controllerInfo = controllers.get(key);
+        if (controllerInfo == null) {
+            controllerInfo = specialControllers.get(key);
+            //todo checks with regex
+        }
         Method method = controllerInfo.method;
-        Parameter[] params = method.getParameters();
+        List<Object> args = getMethodParams(request, method);
+        writeResponse(response, controllerInfo, args);
+    }
+
+    private static void writeResponse(HttpServletResponse response, ControllerInfo controllerInfo, List<Object> args) throws IOException {
+        Object res;
+        try {
+            res = controllerInfo.invoke(args);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        PrintWriter out = response.getWriter();
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        out.print(res);
+        out.flush();
+    }
+
+    private static List<Object> getMethodParams(HttpServletRequest request, Method method) throws IOException {
         List<Object> args = new ArrayList<>();
+        Parameter[] params = method.getParameters();
+
         for (Parameter param : params) {
+            Object p = null;
             if (param.isAnnotationPresent(RequestBody.class)) {
-                String paramName = param.getName();
-                request.getInputStream();
+               BufferedReader reader = request.getReader();
+               StringBuilder res = new StringBuilder();
+               String line = reader.readLine();
+               while (line != null) {
+                   res.append(line);
+                   line = reader.readLine();
+               }
+               p = new Gson().fromJson(res.toString(), param.getType());
             } else if (param.isAnnotationPresent(PathVariable.class)) {
                 String paramName = param.getName();
-                param = null; //add param from path create regex for matching
-
+                param = null; //todo add param from path create regex for matching
             }
-            args.add(param);
+            args.add(p);
         }
+        return args;
     }
 
     @Override
